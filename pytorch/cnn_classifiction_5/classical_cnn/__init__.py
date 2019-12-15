a02d2cd5ee94d91682312fa14493966e7442502b
sxp <sunxianpeng@xiaohulu.com>
2019-12-15 19:06:14 +0800

    update

---
+++ pytorch/cnn_classifiction_5/classical_cnn/__init__.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+"""
+@author: sunxianpeng
+@file: __init__.py.py
+@time: 2019/12/15 19:05
+"""
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
